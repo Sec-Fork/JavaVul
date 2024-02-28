95f157ad63eaa086ff88d63ac94f6c5d3b5c5df2
lokerxx <you@example.com>
2024-02-28 17:33:28 +0800

    新增durid SQL防火墙的检测误报

--- druid_unauthorized/src/main/java/com/myapp/MyApplication.java
+++ druid_unauthorized/src/main/java/com/myapp/MyApplication.java
@@ -1,9 +1,11 @@
 package com.myapp;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.myapp.mapper")
 public class MyApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class, args);
